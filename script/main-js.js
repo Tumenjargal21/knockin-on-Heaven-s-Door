f1074fb59cd2eff56eb6b16aa2d575975919bf2e
Tumenjargal21 <tumetume536@yahoo.com>
2016-08-13 12:22:32 +0900

    Add files via upload

---
+++ script/main-js.js
@@ -0,0 +1,11 @@
+function open_navbarMenu(){
+    	document.getElementById("side-navigationMenu").style.width = "250px";
+    $(document).ready(function(){
+    $("side-navBar").click(function(){
+        $("side-navBar").hide();
+        });
+      });
+    }
+function close_navbarMenu(){
+    	document.getElementById("side-navigationMenu").style.width="0px";
+    }
